992b6314a693a6574cd5488c819ac128db2493bc
BouncyElf <17710281664@163.com>
2020-05-19 22:58:23 +0800

    [problem]: add leetcode 138 https://leetcode-cn.com/problems/copy-list-with-random-pointer/

---
+++ problem/deep_copy_random_list.go
@@ -0,0 +1,69 @@
+package problem
+
+/*
+问题：
+深拷贝带有随机指针的链表
+https://leetcode-cn.com/problems/copy-list-with-random-pointer/
+*/
+
+type Node struct {
+	Val    int
+	Next   *Node
+	Random *Node
+}
+
+// 思路1: 使用map映射老指针与新指针
+func copyRandomList1(head *Node) *Node {
+	if head == nil {
+		return nil
+	}
+	res, now, m := new(Node), new(Node), map[*Node]*Node{}
+	for temp := head; temp != nil; temp = temp.Next {
+		if temp == head {
+			res.Val = temp.Val
+			now = res
+		} else {
+			now.Val = temp.Val
+		}
+		m[temp] = now
+		if temp.Next != nil {
+			now.Next = new(Node)
+			now = now.Next
+		}
+	}
+	for origin, new := head, res; origin != nil; origin, new = origin.Next, new.Next {
+		if origin.Random == nil {
+			continue
+		}
+		new.Random = m[origin.Random]
+	}
+	return res
+}
+
+// 思路2: 直接将老的指针插入到新的指针之后
+func copyRandomList2(head *Node) *Node {
+	if head == nil {
+		return nil
+	}
+	for temp := head; temp != nil; {
+		next := temp.Next
+		temp.Next = &Node{
+			Val:  temp.Val,
+			Next: next,
+		}
+		temp = next
+	}
+	for temp := head; temp != nil && temp.Next != nil; temp = temp.Next.Next {
+		if temp.Random == nil {
+			continue
+		}
+		temp.Next.Random = temp.Random.Next
+	}
+	res := head.Next
+	for temp := head; temp.Next != nil; {
+		next := temp.Next
+		temp.Next = temp.Next.Next
+		temp = next
+	}
+	return res
+}
