5620334400dcffd855c21a048ac126decaf878fc
BouncyElf <17710281664@163.com>
2020-05-23 13:11:02 +0800

    feat: add more common method on TreeNode

--- problem/common.go
+++ problem/common.go
@@ -1,7 +1,9 @@
 package problem
 
 import (
+	"container/list"
 	"fmt"
+	"strconv"
 )
 
 type ListNode struct {
@@ -24,6 +26,51 @@ type TreeNode struct {
 	Val   int
 	Left  *TreeNode
 	Right *TreeNode
+}
+
+func (node *TreeNode) GetVal() string {
+	if node == nil {
+		return "<nil>"
+	}
+	return strconv.Itoa(node.Val)
+}
+
+func (node *TreeNode) String() string {
+	return fmt.Sprintf("Val:%s, Left:%s, Right:%s",
+		node.GetVal(), node.Left.GetVal(), node.Right.GetVal())
+}
+
+func buildTree(data []int, now int) *TreeNode {
+	if now < 0 || now >= len(data) {
+		return nil
+	}
+	node := &TreeNode{
+		Val:   data[now],
+		Left:  buildTree(data, now*2+1),
+		Right: buildTree(data, now*2+2),
+	}
+	return node
+}
+
+func (head *TreeNode) Print() {
+	if head == nil {
+		return
+	}
+	q := list.New()
+	q.PushBack(head)
+	for level := 0; q.Len() != 0; level++ {
+		length := q.Len()
+		for range make([]struct{}, length) {
+			now := q.Remove(q.Front()).(*TreeNode)
+			fmt.Println("level:", level, now)
+			if now.Left != nil {
+				q.PushBack(now.Left)
+			}
+			if now.Right != nil {
+				q.PushBack(now.Right)
+			}
+		}
+	}
 }
 
 func max(a, b int) int {
